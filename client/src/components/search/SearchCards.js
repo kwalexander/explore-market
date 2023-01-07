076f80af3807f10c2f22d9cd876d8e7c0e297224
kabirfaisal1 <md.kabirfaisal@outlook.com>
2023-01-07 13:11:43 -0500

    updated search card

--- client/src/components/search/SearchCards.js
+++ client/src/components/search/SearchCards.js
@@ -1,46 +1,51 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup"
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { MdFavorite } from "react-icons/md";
+import Auth from '../../utils/auth'
 
 
-function ProjectCards(props) {
-    return (
+function SearchCards(props) {
+  return (
+    <>
       <Card data-test-id={props.title} className="project-card-view">
         <Card.Img variant="top" src={props.imgPath} alt="card-img" />
         <Card.Body>
+
           <Card.Title>{props.title}</Card.Title>
           <Card.Text style={{ textAlign: "justify" }}>
             {props.description}
           </Card.Text>
-          <Card.Text style={{ textAlign: "justify" }}>
-            {props.price}
-          </Card.Text>
-          <Button
-              variant="primary"
-              href={props.siteLink}
-              target="_blank"
-              style={{ marginLeft: "10px" }}
-            >
-              <MdFavorite /> &nbsp;
-              
-            </Button>
-          {"\n"}
-          {"\n"}
-          {!props.isBlog && props.siteLink && (
-            <Button
-              variant="primary"
-              href={props.siteLink}
-              target="_blank"
-              style={{ marginLeft: "10px" }}
-            >
-              <CgWebsite /> &nbsp;
-              {"Site Link"}
-            </Button>
-          )}
+                <Card.Subtitle style={{ textAlign: "justify" }}>
+                  {props.price}
+                </Card.Subtitle>
+                <Button
+                  variant="primary"
+                  href={props.siteLink}
+                  target="_blank"
+                  style={{ marginLeft: "10px" }}
+                >
+                <MdFavorite /> &nbsp;
+                </Button>
+                {"\n"}
+                {"\n"}
+                {!props.isBlog && props.siteLink && (
+                  <Button
+                    variant="primary"
+                    href={props.siteLink}
+                    target="_blank"
+                    style={{ marginLeft: "10px" }}
+                  >
+                  <CgWebsite /> &nbsp;
+                    {"Site Link"}
+                  </Button>
+                )}   
+            
         </Card.Body>
       </Card>
-    );
-  }
-  export default ProjectCards;
+    </>
+  );
+}
+export default SearchCards;
