f9034af9c140c5718a46d7f5d8aace68edb971e4
Kaspar Alexander <kaspar.alexander@gmail.com>
2023-01-09 19:42:52 -0500

    home primary buttons updates

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,25 +1,37 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
 import { CgDisplayFlex } from 'react-icons/cg';
 import Particle from "../Particle";
 import travel from "./travel.webp";
 import product from "./product.webp";
 import styles from './home.module.scss';
-
+import { Link } from 'react-router-dom'
+import {
+  SiYourtraveldottv,
+  SiProducthunt
+} from 'react-icons/si'
 
 function Home() {
-
+  const [showModal, setShowModal] = useState(false)
   return (
 
     <div className={`d-flex vh-100 ${styles.root}`}>
       <div className='d-flex'>
         <div className='m-auto'>
           <h1>Travel</h1>
-          <button className='btn btn-primary'>Search</button>
+          <button className='btn btn-primary'
+            as={Link}
+            to='/Product'
+            onClick={() => setShowModal(false)}
+          >
+            <SiProducthunt
+              style={{ marginBottom: '2px' }}
+            />{' '}
+            Search</button>
         </div>
       </div>
       <div className='d-flex'>
-      <div className='m-auto'>
+        <div className='m-auto'>
           <h1>Product</h1>
           <button className='btn btn-primary'>Search</button>
         </div>
